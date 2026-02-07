6bea955af7eaf972dc5ad83e4a62119550469ed3
Bpost129 <Bpost129@yahoo.com>
2026-02-07 18:17:24 -0500

    conditionally render board section to prevent render error on logout

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -17,16 +17,21 @@ const Landing = ({ user }) => {
   }, [])
 
   return (
+
     <main className={styles.container}>
       <h1>WELCOME TO BOARDUM</h1>
-      <h4 className={styles.caption}>Create a new board from scratch or try out a template</h4>
-      <section className={styles.templates}>
-        {boards.map(board => 
-          <div key={board._id}>
-            <h3>{board.title}</h3>
-          </div>
-        )}
-      </section>
+      {user && 
+        <>
+          <h4 className={styles.caption}>Create a new board from scratch or try out a template</h4>
+          <section className={styles.templates}>
+            {boards.map(board => 
+              <div key={board._id}>
+                <h3>{board.title}</h3>
+              </div>
+            )}
+          </section>
+        </>
+      }
     </main>
   )
 }
